0a74599283b87a78571892912e1e3848de18dc21
pawitran413 <aminbagus88@gmail.com>
2025-01-05 22:03:55 +0700

    penyesuaian navbar hp

--- js/own-navbar.js
+++ js/own-navbar.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('tombol-navbar-hp');
-const navbar = document.getElementById('navbar-hp')
+const navbar = document.getElementById('container-navbar-hp')
 const garisSatu = document.getElementById('line1');
 const garisDua = document.getElementById('line2');
 const garisTiga = document.getElementById('line3');
@@ -10,14 +10,14 @@ const garisTiga = document.getElementById('line3');
 button.addEventListener('click', () => {
     if (navbar.classList.contains('hidden')) {
         navbar.classList.remove('hidden');
-        navbar.style.left = '100vw';
+        navbar.style.width = '0vw';
         garisSatu.style.transform = 'rotate(0deg)';
         garisDua.style.opacity = '100';
         garisTiga.style.transform = 'rotate(0deg)';
     } else {
         navbar.classList.add('hidden');
-        navbar.style.left = '50vw';
-        navbar.style.transition = 'left 0.5s ease-in-out';
+        navbar.style.width = '50vw';
+        navbar.style.transition = 'all 0.4s ease-in-out';
         garisSatu.style.transform = 'rotate(-45deg) translate(-5px, 5px)';
         garisDua.style.opacity = '0';
         garisTiga.style.transform = 'rotate(45deg) translate(-5px, -5px)';
